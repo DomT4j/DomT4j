227a65fa215cc1b0d8f6a2dde80531c23075efb4
jGo-java-General-operations <staff@jgo.cloud>
2019-01-06 18:19:20 +0100

    Comments update

--- DomT4j/src/test/java/com/github/domt4j/DomT4jTest.java
+++ DomT4j/src/test/java/com/github/domt4j/DomT4jTest.java
@@ -5,7 +5,11 @@ public class DomT4jTest {
 public static void main(String[] args) {
 	
 	
-	
+	// prossimo passo : gestire la multi utenza, in attesa che si risolva in jGo 1.0.9
+	// la feature che ci permette di condividere oggetti creando parametri automatici
+	// in base ai campi dell'oggetto condiviso, questo approccio non lo utilizzeremo però
+	// per la creazione dei nodi, quindi con il comando create, ma ci servirà per altre features
+	// di DomT4j, e comunue è un di + jGo.
 	// adesso ci occupiamo di questo primo bug
 	
 	// dobbiamo eliminare tutti i parametri aggiunti al comando, una volta che l'oggetto condiviso viene meno
